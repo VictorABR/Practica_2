75b3142591289f45cee0cd70e9fae165616f9565
victor.bernardino@uabc.edu.mx <victor.bernardino@uabc.edu.mx>
2021-08-31 19:17:20 -0700

    Agregar archivos de proyecto.

---
+++ Calculo perimetro/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculo_perimetro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 10; int b = 10; int c = 7;
+            int p = a + b + c;
+            Console.WriteLine("El calculo del perometro es:" + p);
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Ejercicio_String/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Nombre, Ciudad;
+            Console.WriteLine("Escribre tu nombre:");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("Escribre tu ciudad");
+            Ciudad = Console.ReadLine();
+            Console.WriteLine("Hola "+ Nombre +"Bienvenido a " +Ciudad);
+            Console.WriteLine("Preciona cualquier tecla");
+            Console.ReadKey();  
+
+        }
+    }
+}
